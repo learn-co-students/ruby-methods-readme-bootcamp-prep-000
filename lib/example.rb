58b31d54482004d562389d1450ebb2f2c2be09aa
Michael Rakitzis <michael.rakitzis@gmail.com>
2020-09-09 17:13:12 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,11 @@
-# Use this space to code along with the readme
 
+def hello_world_five_times
 phrase = "Hello World!"
 puts phrase
 puts phrase
 puts phrase
 puts phrase
 puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+end
+
+hello_world_five_times
